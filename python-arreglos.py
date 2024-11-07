68b2ee1aa0a1e03d4cf3a31dd17afd08ce1c0d4f
monzonpuntoexe <monzonfranco95@gmail.com>
2024-11-07 04:16:40 -0300

    codigo-comentado

--- python-arreglos.py
+++ python-arreglos.py
@@ -1,18 +1,27 @@
 # Arreglos
 
 arreglos = ["a", "p", "g", "u", "r"]
-compra = ["3 perros", "2 mocos", "cositas", "mas perros", "una Speed"]
+compra = [
+    "Compras de a",
+    "Compras de p",
+    "Compras de g",
+    "Compras de u",
+    "Compras de r",
+]
 
 # Buscar un dato del arreglo:
-for caca in range(len(arreglos)):
-    if "g" == arreglos[caca]:
-        print(caca)
-        print(arreglos[caca])
-        print(compra[caca])
+# cat indica el indice, por convension usamos la letra "i" de "Index"
+for cat in range(len(arreglos)):
+    # Iteramos la cantidad de elementos que tenga el arreglo
+    if "g" == arreglos[cat]:  # En la vuelta que encuentre "g" ingresa al bloque
+        print(cat)
+        # imprime "g"
+        print(arreglos[cat])  # arreglos[2]
+        print(compra[cat])  # compra[2]
 
 # Otra forma de Busqueda:
-# Caca toma la forma de los datos que tiene "arreglos"
-for caca in arreglos:
-    if caca == "g":
-        print(caca)
+# cat toma la forma de los datos que tiene "arreglos"
+for cat in arreglos:
+    if cat == "g":
+        print(cat)
         print("encontraste la G perro")
--- python-funciones.py
+++ python-funciones.py
@@ -1,5 +1,11 @@
 # funciones con parametros
 
+# Los parametros cuando se CUANDO SE DEFINEN pueden tener cualquier nombre
+# Lo mejor es colocarle un nombre descriptivo y
+# que tenga que ver con el contexto
+
+# Ejemplo:
+
 
 def suma(x, y):
     resultado = x + y
@@ -7,28 +13,35 @@ def suma(x, y):
     return resultado
 
 
+# numeros que definie el usuario
+
+
 numero = int(input("Ingre un numero 1: "))
 numero2 = int(input("Ingre un numero 2: "))
 
-
+# variables hard Codeados
 numero4 = 4
 numero3 = 6
+
+# return nos devuelve el dato que tiene "resultado"
+# Para que luego podemos usar ese dato para lo que necesitemos
+# Para guardar el dato que nos devuelve return en este caso "resultado"
+# Lo podemos guardar en una variable
 
 resultado_1 = suma(numero, numero2)
 resultado_2 = suma(numero4, numero3)
 
-
+# printeamos
 print(resultado_1)
 print(resultado_2)
-
-# guardar_resultado_return = suma()
-# print(guardar_resultado_return)
 
 
 if resultado_2 == 10:
     print("El resultado es igual a 10")
 else:
     print("No es igual a 10")
+
+# Implementamos recursividad
 
 
 def validar():
@@ -41,5 +54,6 @@ def validar():
 
 validacion = validar()
 
-# Importate: Guardar el resultado de la funcion que tiene "return" en una variable si se quiere printear o
-# Si se quiere utilizar el resultado de esa funcion en otra operacion
+# Importate: Guardar el resultado de la funcion que tiene "return" en una variable
+# si se quiere printear o
+# si se quiere utilizar el resultado de esa funcion en otra operacion
